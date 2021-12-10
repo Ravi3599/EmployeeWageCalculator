6dbedebb894742bfa454a99772640f0aaed47e85
Ravi3599 <94590162+Ravi3599@users.noreply.github.com>
2021-12-10 19:25:29 +0530

    MODIFIED: days

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -2,7 +2,7 @@ public class EmpWageBuilderUC1 {
 	 public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
-	 public static final int NUM_OF_WORKING_DAYS = 2;
+	 public static final int NUM_OF_WORKING_DAYS = 20;
 
 	public static void main(String[] args){
 
