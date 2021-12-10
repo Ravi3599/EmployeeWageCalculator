40b7a3f9c3962bd2fd11ef27ccbec4edf39c22f0
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-10 11:16:12 +0530

    MODIFIED: some changes

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -5,10 +5,6 @@ public class EmpWageBuilderUC1 {
 
 	public static void main(String[] args){
 
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-
 	public static void main(String[] args){
 
 			int empHrs=0;
